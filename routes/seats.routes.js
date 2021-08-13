d3d9ea49c9d60f3a3900851f4b8b4b0a4c2dd4ec
Michał Micherda <mmicherdakr@gmail.com>
2021-08-13 10:22:35 +0200

    Add correct routes

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,61 +1,19 @@
 const express = require('express');
-const db = require('../db');
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
 
-router.route('/seats').get((req, res) => {
-  res.json(db.seats)
-});
+const SeatsController = require('../controllers/seats.controller');
 
-router.route('/seats/random').get((req, res) => {
-  res.json(db.seats[Math.floor(Math.random() * db.seats.length)]);
-});
+router.get('/seats', SeatsController.getAll);
 
-router.route('/seats/:id').get((req, res) => {
-  res.json(db.seats[req.params.id])
-})
+router.get('/seats/random', SeatsController.getRandom);
 
-router.route('/seats/').post((req, res) => {
-  const obj = {
-    id: uuidv4(),
-    day: req.body.day,
-    seat: req.body.seat,
-    client: req.body.client,
-    email: req.body.email,
-  }
-  if (db.seats.some(chosenSeat => (chosenSeat.day == req.body.day && chosenSeat.seat == req.body.seat))) {
-    return res.status(404).json({ message: 'This slot is aleady taken' });
-  } else {
-    db.seats.push(obj);
-    return res.json(db.seats);
-  }
-});
+router.get('/seats/:id', SeatsController.getOne);
 
-router.route('/seats/:id').put((req, res) => {
-  db.seats.forEach(element => {
-    if (element.id == req.params.id) {
-      element.day = req.body.day,
-        element.seats = req.body.seats,
-        element.client = req.body.client,
-        element.email = req.body.email
-    }
-  });
-  return res.json({
-    message: 'ok'
-  });
-});
+router.post('/seats/', SeatsController.create);
 
-router.route('/seats/:id').delete((req, res) => {
-  db.seats.forEach(element => {
-    if (element.id == req.params.id) {
-      const index = db.seats.indexOf(element);
-      db.seats.splice(index, 1);
-    }
-  });
-  return res.json({
-    message: 'ok'
-  });
-});
+router.put('/seats/:id', SeatsController.changeOne);
+
+router.delete('/seats/:id', SeatsController.deleteOne);
 
 module.exports = router;
 
--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,54 +1,18 @@
 const express = require('express');
-const db = require('../db');
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
 
-router.route('/testimonials').get((req, res) => {
-  res.json(db.testimonials)
-});
+const TestimonialsController = require('../controllers/testimonials.controller');
 
-router.route('/testimonials/random').get((req, res) => {
-  res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
-});
+router.get('/testimonials', TestimonialsController.getAll);
 
-router.route('/testimonials/:id').get((req, res) => {
-  res.json(db.testimonials[req.params.id])
-})
+router.get('/testimonials/random', TestimonialsController.getRandom);
 
-router.route('/testimonials/').post((req, res) => {
-  const obj = {
-    id: uuidv4(),
-    author: req.body.author,
-    text: req.body.text
-  }
-  db.testimonials.push(obj);
-  return res.json({
-    message: 'ok'
-  });
-});
+router.get('/testimonials/:id', TestimonialsController.getOne);
 
-router.route('/testimonials/:id').put((req, res) => {
-  db.testimonials.forEach(element => {
-    if (element.id == req.params.id) {
-      element.author = req.body.author;
-      element.text = req.body.text;
-    }
-  });
-  return res.json({
-    message: 'ok'
-  });
-});
+router.post('/testimonials/', TestimonialsController.create);
 
-router.route('/testimonials/:id').delete((req, res) => {
-  db.testimonials.forEach(element => {
-    if (element.id == req.params.id) {
-      const index = db.testimonials.indexOf(element);
-      db.testimonials.splice(index, 1);
-    }
-  });
-  return res.json({
-    message: 'ok'
-  });
-});
+router.put('/testimonials/:id', TestimonialsController.changeOne);
+
+router.delete('/testimonials/:id', TestimonialsController.deleteOne);
 
 module.exports = router;
